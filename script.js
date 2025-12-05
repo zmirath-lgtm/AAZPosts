2e213f685646e27f91818ad16c33dd9e521c4c58
zmirath-lgtm <zmirath@gmail.com>
2025-12-05 12:43:12 +0330

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-// بارگذاری مقالات از localStorage
-let posts = JSON.parse(localStorage.getItem("posts")) || [];
-
-const container = document.getElementById("posts");
-
-posts.forEach(post=>{
-  const div = document.createElement("div");
-  div.className="post";
-  div.innerHTML = `
-    <img src="${post.cover}" alt="${post.title}">
-    <h2>${post.title}</h2>
-    <p>${post.text}</p>
-    <button onclick="location.href='article.html?id=${post.id}'">ادامه مطلب</button>
-  `;
-  container.appendChild(div);
-});
+// بارگذاری مقالات از posts.json
+fetch("posts.json")
+  .then(res => res.json())
+  .then(posts => {
+    const container = document.getElementById("posts");
+    posts.forEach(post => {
+      const div = document.createElement("div");
+      div.className = "post";
+      div.innerHTML = `
+        <img src="${post.cover}" alt="${post.title}">
+        <h2>${post.title}</h2>
+        <p>${post.text}</p>
+        <button onclick="location.href='article.html?id=${post.id}'">ادامه مطلب</button>
+      `;
+      container.appendChild(div);
+    });
+  })
+  .catch(err => console.error("مشکل در بارگذاری مقالات:", err));
